ba1d9bd5c1fde6f5f2d7c825353da7d5372efd11
LitianyuJob <LitianyuJob@163.com>
2016-09-02 14:35:23 +0800

    测试

---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by wu xinglin on 16/9/2.
+//  Copyright © 2016年 wu xinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by wu xinglin on 16/9/2.
+//  Copyright © 2016年 wu xinglin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor blueColor];
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
